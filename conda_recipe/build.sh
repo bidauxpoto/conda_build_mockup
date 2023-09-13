db4181532ca1cb258056f3232798d4586379d928
Chiara Cicconetti <76106349+chiaracicco@users.noreply.github.com>
2023-09-13 12:56:25 +0200

    Update build.sh

--- conda_recipe/build.sh
+++ conda_recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #build.sh must be adapted to the logic in /src
-#this small template is ok for single file scripts
+#this small template is suitable for single file scripts
 mkdir -p $PREFIX/bin/
-cp MyScript.py $PREFIX/bin/
+cp MyScript.py $PREFIX/bin/MyScript
 chmod +x $PREFIX/bin/
